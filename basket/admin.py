ea82ac7d3fc8006d799b4442cb552b582bdfc9db
plentik <bplentik@gmail.com>
2023-06-28 11:39:44 +0300

    добавил модели

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Motorcycle)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name', 'category']
+
+
+@admin.register(models.MainPart)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name', 'motorcycle']
+
+
+@admin.register(models.Part)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name', 'main_part', 'price', 'number']
--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Категория')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Motorcycle(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Мото/Двигатели')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    def __str__(self):
+        return self.name
+
+
+class MainPart(models.Model):
+    motorcycle = models.ForeignKey(Motorcycle, on_delete=models.CASCADE, verbose_name='Мото/Двигатель')
+    name = models.CharField(max_length=255, verbose_name='Основная запчасть')
+
+    def __str__(self):
+        return self.name
+
+
+class Part(models.Model):
+    main_part = models.ForeignKey(MainPart, on_delete=models.CASCADE, verbose_name='Основная запчасть')
+    number = models.IntegerField(verbose_name='№')
+    name = models.CharField(max_length=300, verbose_name='Наименование')
+    price = models.IntegerField(default=0, verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
